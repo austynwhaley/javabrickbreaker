6c541cfe2930cc48d1c97116d4a4a231e9866521
austynwhaley <austyn_whaley@yahoo.com>
2022-06-06 11:48:54 -0400

    inital commit

---
+++ brickbreaker/src/brickbreaker/BBMain.java
@@ -0,0 +1,12 @@
+package brickbreaker;
+
+public class BBMain {
+	//state the size of class
+	public static final int WIDTH = 640, HEIGHT = 480;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
