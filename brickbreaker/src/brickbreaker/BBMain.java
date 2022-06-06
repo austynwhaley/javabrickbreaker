4780ec7ce8f16068a7b6a94f34c8e145c5b68ed5
austynwhaley <austyn_whaley@yahoo.com>
2022-06-06 12:17:20 -0400

    added jframe

--- brickbreaker/src/brickbreaker/BBMain.java
+++ brickbreaker/src/brickbreaker/BBMain.java
@@ -1,12 +1,20 @@
 package brickbreaker;
 
+import javax.swing.JFrame;
+
 public class BBMain {
 	//state the size of class
 	public static final int WIDTH = 640, HEIGHT = 480;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//code for starting window
+		JFrame theFrame = new JFrame("Brick Breaker");
+		theFrame.setLocationRelativeTo(null);
+		theFrame.setResizable(false);
+		theFrame.setSize(WIDTH, HEIGHT);
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.setVisible(true);
 	}
 
 }
